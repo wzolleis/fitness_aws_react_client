f2fb84524b46b3f06691623dfc505e840d1899d4
Wolfgang Zolleis <wzolleis@web.de>
2017-08-18 14:54:38 +0200

    logout und userToken im state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, {Component} from 'react';
 import {withRouter, Link} from 'react-router-dom';
-import {Nav, Navbar} from 'react-bootstrap';
+import {Nav, Navbar, NavItem} from 'react-bootstrap';
 
 import './App.css';
 import Routes from './Routes';
 import RouteNavItem from './components/RouteNavItem';
 
 class App extends Component {
+    updateUserToken = (userToken) => {
+        this.setState({
+            userToken: userToken
+        });
+    };
     handleNavLink = (event) => {
         event.preventDefault();
         this.props.history.push(event.currentTarget.getAttribute('href'));
+    };
+    handleLogout = (event) => {
+        this.updateUserToken(null);
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userToken: null,
+        };
     }
 
     render() {
+        const childProps = {
+            userToken: this.state.userToken,
+            updateUserToken: this.updateUserToken,
+        };
+
         return (
             <div className="App container">
                 <Navbar fluid collapseOnSelect>
@@ -25,12 +46,16 @@ class App extends Component {
 
                     <Navbar.Collapse>
                         <Nav pullRight>
-                            <RouteNavItem onClick={this.handleNavLink} href="/signup">Signup</RouteNavItem>
-                            <RouteNavItem onClick={this.handleNavLink} href="/login">Login</RouteNavItem>
+                            {this.state.userToken
+                                ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                                : [<RouteNavItem key={1} onClick={this.handleNavLink}
+                                                 href="/signup">Signup</RouteNavItem>,
+                                    <RouteNavItem key={2} onClick={this.handleNavLink}
+                                                  href="/login">Login</RouteNavItem>]}
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
-                <Routes/>
+                <Routes childProps={childProps}/>
             </div>
         );
     }

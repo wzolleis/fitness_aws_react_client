1db7a102e1990317770fcfffdcd400d6dcd3d9af
Wolfgang Zolleis <wzolleis@web.de>
2017-08-18 15:43:22 +0200

    Neue Exercise erzeugen (ohne aws bis jetzt)

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 import Home from './containers/Home';
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import AppliedRoute from "./components/AppliedRoute";
 import Signup from "./containers/Signup";
+import NewExercise from "./components/NewExercise";
 
 export default ({childProps}) => (
     <Switch>
         <AppliedRoute path="/" exact component={Home} props={childProps}/>
         <AppliedRoute path="/login" exact component={Login} props={childProps}/>
         <AppliedRoute path="/signup" exact component={Signup} props={childProps}/>
+        <AppliedRoute path="/exercises/new" exact component={NewExercise} props={childProps}/>
         <Route component={NotFound}/>
     </Switch>
 );
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
     cognito: {
         USER_POOL_ID: 'eu-central-1_V5TPV8THj',
         APP_CLIENT_ID: '1u8a4dvkn40q96rt2ri1cq9m6h'

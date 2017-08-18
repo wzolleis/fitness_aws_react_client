6d407b82fda4b2c3e22f5c14aa98814fd131a8f1
Wolfgang Zolleis <wzolleis@web.de>
2017-08-18 14:04:23 +0200

    routes setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar } from 'react-bootstrap';
 import './App.css';
+import Routes from './Routes';
 
 class App extends Component {
     render() {
@@ -10,11 +11,12 @@ class App extends Component {
                 <Navbar fluid collapseOnSelect>
                     <Navbar.Header>
                         <Navbar.Brand>
-                            <Link to="/">Scratch</Link>
+                            <Link to="/">Exercises</Link>
                         </Navbar.Brand>
                         <Navbar.Toggle />
                     </Navbar.Header>
                 </Navbar>
+                <Routes />
             </div>
         );
     }
---
+++ src/containers/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="Home">
+                <div className="lander">
+                    <h1>Fitness</h1>
+                    <p>Get Fit and in shape with my little app</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;

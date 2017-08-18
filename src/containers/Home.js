a1f582b669fb84f15a2f0f0a1c0ded3793ccf4c6
Wolfgang Zolleis <wzolleis@web.de>
2017-08-18 18:13:42 +0200

    Preparation - list exercises

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,17 +1,56 @@
 import React, { Component } from 'react';
+import {withRouter} from 'react-router-dom';
+import {
+    PageHeader,
+    ListGroup,
+} from 'react-bootstrap';
 import './Home.css';
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoading: false,
+            notes: [],
+        };
+    }
+
+    renderExercisesList(notes) {
+        return null;
+    }
+
+    renderLander() {
+        return (
+            <div className="lander">
+                <h1>Scratch</h1>
+                <p>A simple note taking app</p>
+            </div>
+        );
+    }
+
+    renderExercises() {
+        return (
+            <div className="exercises">
+                <PageHeader>Your Exercises</PageHeader>
+                <ListGroup>
+                    {!this.state.isLoading
+                    && this.renderExercisesList(this.state.notes)}
+                </ListGroup>
+            </div>
+        );
+    }
+
     render() {
         return (
             <div className="Home">
-                <div className="lander">
-                    <h1>Fitness</h1>
-                    <p>Get Fit and in shape with my little app</p>
-                </div>
+                {this.props.userToken === null
+                    ? this.renderLander()
+                    : this.renderExercises()}
             </div>
         );
     }
 }
 
-export default Home;
+export default withRouter(Home);

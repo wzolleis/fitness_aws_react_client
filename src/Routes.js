7b17eef6248d98055d85417030c99162af28e1f7
Wolfgang Zolleis <wzolleis@web.de>
2017-08-19 20:43:20 +0200

    redirect to login if necessary

--- src/Routes.js
+++ src/Routes.js
@@ -8,14 +8,16 @@ import AppliedRoute from "./components/AppliedRoute";
 import Signup from "./containers/Signup";
 import NewExercise from "./components/NewExercise";
 import Exercises from "./containers/Exercises";
+import AuthenticatedRoute from './components/AuthenticatedRoute';
+import UnauthenticatedRoute from './components/UnauthenticatedRoute';
 
 export default ({childProps}) => (
     <Switch>
         <AppliedRoute path="/" exact component={Home} props={childProps}/>
-        <AppliedRoute path="/login" exact component={Login} props={childProps}/>
-        <AppliedRoute path="/signup" exact component={Signup} props={childProps}/>
-        <AppliedRoute path="/exercises/new" exact component={NewExercise} props={childProps}/>
-        <AppliedRoute path="/exercises/:id" exact component={Exercises} props={childProps}/>
+            <UnauthenticatedRoute path="/login" exact component={Login} props={childProps}/>
+            <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps}/>
+            <AuthenticatedRoute path="/exercises/new" exact component={NewExercise} props={childProps}/>
+            <AuthenticatedRoute path="/exercises/:id" exact component={Exercises} props={childProps}/>
         <Route component={NotFound}/>
     </Switch>
 );

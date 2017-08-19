4424c78f1bf65b9f9a533f914a1b0d8aba980ae0
Wolfgang Zolleis <wzolleis@web.de>
2017-08-19 22:16:48 +0200

    code splitting and loadingComponent

--- src/Routes.js
+++ src/Routes.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 
-import Home from './containers/Home';
-import NotFound from "./containers/NotFound";
-import Login from "./containers/Login";
+//import Home from './containers/Home';
+// import NotFound from "./containers/NotFound";
+// import Login from "./containers/Login";
+// import Signup from "./containers/Signup";
+// import NewExercise from "./components/NewExercise";
+// import Exercises from "./containers/Exercises";
+import asyncComponent from './components/AsyncComponent';
 import AppliedRoute from "./components/AppliedRoute";
-import Signup from "./containers/Signup";
-import NewExercise from "./components/NewExercise";
-import Exercises from "./containers/Exercises";
 import AuthenticatedRoute from './components/AuthenticatedRoute';
 import UnauthenticatedRoute from './components/UnauthenticatedRoute';
 
+
+const AsyncHome = asyncComponent(() => import('./containers/Home'));
+const AsyncLogin = asyncComponent(() => import('./containers/Login'));
+const AsyncExercises = asyncComponent(() => import('./containers/Exercises'));
+const AsyncSignup = asyncComponent(() => import('./containers/Signup'));
+const AsyncNewExercise = asyncComponent(() => import('./containers/NewExercise'));
+const AsyncNotFound = asyncComponent(() => import('./containers/NotFound'));
+
 export default ({childProps}) => (
     <Switch>
-        <AppliedRoute path="/" exact component={Home} props={childProps}/>
-            <UnauthenticatedRoute path="/login" exact component={Login} props={childProps}/>
-            <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps}/>
-            <AuthenticatedRoute path="/exercises/new" exact component={NewExercise} props={childProps}/>
-            <AuthenticatedRoute path="/exercises/:id" exact component={Exercises} props={childProps}/>
-        <Route component={NotFound}/>
+        <AppliedRoute path="/" exact component={AsyncHome} props={childProps}/>
+        <UnauthenticatedRoute path="/login" exact component={AsyncLogin} props={childProps}/>
+        <UnauthenticatedRoute path="/signup" exact component={AsyncSignup} props={childProps}/>
+        <AuthenticatedRoute path="/exercises/new" exact component={AsyncNewExercise} props={childProps}/>
+        <AuthenticatedRoute path="/exercises/:id" exact component={AsyncExercises} props={childProps}/>
+        <Route component={AsyncNotFound}/>
     </Switch>
 );

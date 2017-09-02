2a89211e22ed8a238baa57d1ba7e8bde3b3254d5
wz <wz@nowhere.org>
2017-09-02 22:28:42 +0200

    Redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 import './index.css';
+import store from "./Store";
+import {Provider} from 'react-redux'
+
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );

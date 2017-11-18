2ce483a69f1454d12ae32a4bc9f27679023a4955
Wolfgang Zolleis <wzolleis@web.de>
2017-11-18 23:37:15 +0100

    training item dummy page

--- src/containers/TrainingItem.js
+++ src/containers/TrainingItem.js
@@ -3,8 +3,30 @@ import React, {Component} from 'react';
 class TrainingItem extends Component {
     render() {
         return (
-            <div>New Training!</div>
+            <div class="card">
+                <div class="card-header">
+                    <ul class="nav nav-tabs card-header-tabs">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Active</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card-body">
+                    <h4 class="card-title">Card Title</h4>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut,
+                        perspiciatis.</p>
+                    <a href="#" class="btn btn-primary">Read More</a>
+                </div>
+            </div>
+
         )
+
     }
 }
 

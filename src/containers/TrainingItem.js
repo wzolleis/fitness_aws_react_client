c23e03bd85af10ba87f6fd96051e7de9e4470af2
Wolfgang Zolleis <wzolleis@web.de>
2017-11-19 20:15:56 +0100

    class mit className ersetzt

--- src/containers/TrainingItem.js
+++ src/containers/TrainingItem.js
@@ -3,25 +3,25 @@ import React, {Component} from 'react';
 class TrainingItem extends Component {
     render() {
         return (
-            <div class="card">
-                <div class="card-header">
-                    <ul class="nav nav-tabs card-header-tabs">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Active</a>
+            <div className="card">
+                <div className="card-header">
+                    <ul className="nav nav-tabs card-header-tabs">
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#">Active</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Disabled</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Disabled</a>
                         </li>
                     </ul>
                 </div>
-                <div class="card-body">
-                    <h4 class="card-title">Card Title</h4>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut,
+                <div className="card-body">
+                    <h4 className="card-title">Card Title</h4>
+                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut,
                         perspiciatis.</p>
-                    <a href="#" class="btn btn-primary">Read More</a>
+                    <a href="#" className="btn btn-primary">Read More</a>
                 </div>
             </div>
 

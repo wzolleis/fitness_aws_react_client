b06f00b477836feeb939c9935fb3bf925bee4294
Wolfgang Zolleis <wzolleis@web.de>
2017-12-01 00:23:49 +0100

    TrainingItem als Redux-Komponente

--- src/containers/TrainingItem.js
+++ src/containers/TrainingItem.js
@@ -1,6 +1,18 @@
+// @flow weak
 import React, {Component} from 'react';
+import {withRouter} from "react-router-dom";
+import {connect} from "react-redux";
+import type {Exercise, ExerciseId, State, Training} from "../types/index";
+import _ from 'lodash';
 
-class TrainingItem extends Component {
+type TrainingItemProps = {
+    training: Training,
+    allExercises: Exercise[],
+    trainingExercises: Exercise[]
+}
+
+class TrainingItem extends Component<TrainingItemProps> {
+
     render() {
         return (
             <div className="card">
@@ -30,4 +42,28 @@ class TrainingItem extends Component {
     }
 }
 
-export default TrainingItem;
+function mapStateToProps(state: State, customProps) {
+    const id: string = customProps.match.params.id ? customProps.match.params.id : ''; // from current url (react-router)
+    const training: Training = state.training.trainings[id];
+    const allExercises: Exercise[] = state.exercise.exercises;
+    const trainingExercisesIds: ExerciseId[] = training ? training.exercises : [];
+    const trainingExercises: Training[] = _.filter(allExercises, ex => {
+        return trainingExercisesIds.indexOf(ex.id) !== -1;
+    });
+
+    return {
+        isLoading: false,
+        isDeleting: false,
+        training,
+        allExercises: state.exercise.exercises, // alle exercises
+        trainingExercises,
+        initialValues: {
+            name: training ? training.name : '',
+            createdAt: training ? training.createdAt : '',
+            trainingExercises: []
+        }
+    }
+
+}
+
+export default withRouter(connect(mapStateToProps)(TrainingItem));
